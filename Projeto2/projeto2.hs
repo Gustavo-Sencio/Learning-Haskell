1e03fd75b00b5dddbfa8244c95c7af547501ab14
Gustavosencio14 <g171425@dac.unicamp.br>
2024-08-28 19:00:14 -0300

    Making descomprime function

--- Projeto2/projeto2.hs
+++ Projeto2/projeto2.hs
@@ -6,4 +6,11 @@ comprime lista = comprime' lista 1
             | x == (head xs) = comprime' xs (acc + 1)
             | otherwise = (x, acc) : comprime' xs 1
 
+
+descomprime [] = []
+descomprime lista = concat $ map cria_lista lista
+    where
+        cria_lista (x,1) = [x]
+        cria_lista tupla = (fst tupla) : (cria_lista (fst tupla, snd tupla - 1))
+
 main = print(comprime [3,3,3,4,5,6,5,5,5,5,7])
